3b6ad0b422079d73850e68628fdbf83989d89bba
Carsten Rodin <carsten.rodin@gmail.com>
2020-04-21 14:09:26 -0400

    css grid version of landing page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import React, {Component} from 'react'
 import useSWR from 'swr'
 import { render } from 'react-three-fiber'
 import Router from 'next/router'
-import FourGrid from '../layouts/FourGrid'
 
 // function fetcher(url) {
 //   return fetch(url).then(r => r.json());
@@ -23,23 +22,19 @@ class Index extends Component {
   render() {
     return (
       <div className="flex bg-gray-200 w-full h-full">
-        <div className="flex flex-col flex-grow items-stretch m-20">
-          <div className="flex flex-grow items-stretch">
-            <span className="flex-grow flex items-center justify-center m-5 border-black border-2">Spring 2020</span>
-            <span className="flex-grow flex items-center justify-center m-5 border-black border-2">Previous Studios</span>
-          </div>
-          <div className="flex flex-grow items-stretch">
-            <span className="flex-grow flex items-center justify-center m-5 border-black border-2">Media & Bibliography</span>
-            <span className="flex-grow flex items-center justify-center m-5 border-black border-2">Who</span>
-          </div>
+        <div className="flex-grow grid grid-cols-2 gap-8 m-48">
+          <span className="flex items-center justify-center border-black border-2">Spring 2020</span>
+          <span className="flex items-center justify-center border-black border-2">Previous Studios</span>
+          <span className="flex items-center justify-center border-black border-2">Media & Bibliography</span>
+          <span className="flex items-center justify-center border-black border-2">Who</span>
         </div>
         <div>
-            <div className="fixed top-0 left-0 m-2">Search</div>
+            <div className="fixed top-0 left-0 m-6">Search</div>
             <div>
-              <a className="fixed bottom-0 left-0 m-2" href={'http://estudioherreros.com/en/'}>estudio Herreros</a>
+              <a className="fixed bottom-0 left-0 m-6" href={'http://estudioherreros.com/en/'}>estudio Herreros</a>
             </div>
-            <div className="fixed top-0 right-0 m-2">Lang</div>
-            <div className="fixed bottom-0 right-0 m-2">Contact</div>
+            <div className="fixed top-0 right-0 m-6">Lang</div>
+            <div className="fixed bottom-0 right-0 m-6">Contact</div>
         </div>
       </div>
     )
