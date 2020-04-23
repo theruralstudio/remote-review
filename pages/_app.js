15dc2c597f3a3d83ae0f8f85d846ddd810ca1bd0
Carsten Rodin <carsten.rodin@gmail.com>
2020-04-22 21:03:46 -0400

    undo scrolling on archive panel

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ export default function MyApp(props) {
       <div className="flex w-full h-full p-4">
         <div className="flex flex-grow -mx-2">
           <div className={`flex ${open ? 'w-2/5' : 'w-full'} px-2`}>
-            <div className="flex-grow bg-white border-2 border-black overflow-scroll">
+            <div className="flex-grow bg-white border-2 border-black">
               {/* { children } */}
               <Component {...pageProps}/>
             </div>
