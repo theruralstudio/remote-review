4efc469bdad78a6a6458d6bcd1fe8c4e321da49f
Carsten Rodin <carsten.rodin@gmail.com>
2020-04-25 16:25:51 -0400

    default review view to call

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,9 @@ import App from 'next/app'
 import '../styles/main.css'
 
 // FIREBASE
-import { FirebaseContext } from '../utils/firebase'
-import 'firebase/database'
-import { useListVals } from 'react-firebase-hooks/database'
-
-// LAYOUTS
-// import BasicLayout from '../layouts/BasicLayout'
+// import { FirebaseContext } from '../utils/firebase'
+// import 'firebase/database'
+// import { useListVals } from 'react-firebase-hooks/database'
 
 // COMPONENTS
 import ReviewFrame from '../components/ReviewFrame'
@@ -33,9 +30,8 @@ class MyApp extends App {
           border: `2px solid #000000`,
         }
       },
-      view: 'table',
+      view: 'stream',
       open: true,
-      // streamurl: 'https://www.youtube.com/watch?v=YO67a5UvX9k',
     }
   }
 
@@ -47,37 +43,9 @@ class MyApp extends App {
     this.setState({user: v})    
   }
 
-
   render() {
     const { Component, pageProps } = this.props
     const Layout = Component.Layout || Noop
-
-    // const router = useRouter()
-  
-    // const [user, setUser] = useState({ name: 'Anonymous', style: { color: '#000000', background: '#ffffff', border: `2px solid #000000`,} });
-    // const [userCount, setUserCount] = useState(0);
-    // const [view, setView] = useState('table') // if view isn't provided in link, won't work
-    // const [open, setOpen] = useState(true) 
-    // const [streamUrl, setStreamUrl] = useState('https://www.youtube.com/watch?v=YO67a5UvX9k');
-    // FINAL REVIEW: 'https://www.youtube.com/watch?v=Zoe3zSEIAl8'
-  
-    // depending on "open" and "view" router params
-    // render the right page layout
-    // if (router.pathname == '/' ) {
-    //   return (
-    //     <Layout>
-    //       <div className="flex w-full h-full p-4">
-    //         <div className="flex flex-grow -mx-2">
-    //           <div className={`flex w-full px-2`}>
-    //             <div className="flex-grow">
-    //               <Component {...pageProps}/>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>  
-    //     </Layout>      
-    //   )
-    // } else {
     const reviewChildren = this.state.open == true && <ReviewFrame view={this.state.view} setView={this.setView} user={this.state.user} setUser={this.setUser} />
 
     return (
