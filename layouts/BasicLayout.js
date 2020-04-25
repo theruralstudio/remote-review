9cbb9ab534e3edfbc316751213feb11c390d2dd5
Carsten Rodin <carsten.rodin@gmail.com>
2020-04-25 16:20:51 -0400

    table images behind archive panel

--- layouts/BasicLayout.js
+++ layouts/BasicLayout.js
@@ -6,7 +6,7 @@ export default ({children, reviewChildren, view, open}) => {
       <div id="full-page" className="flex w-full h-full overflow-hidden bg-gray-200">
         <div className="flex w-full h-full p-4">
           <div className="flex flex-grow -mx-2">
-            <div className={`flex ${open ? 'md:w-2/6 lg:w-2/6 xl:w-4/12' : 'w-full'} px-2`}>
+            <div className={`flex ${open ? 'md:w-2/6 lg:w-2/6 xl:w-4/12' : 'w-full'} px-2 z-10`}>
               <div className="flex-grow bg-white border-2 border-black">
                 { children }
                 {/* <Component {...pageProps}/> */}
